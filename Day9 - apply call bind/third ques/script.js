4ee4f02b15ebb79a88e755fc722ef723ef4bc5ff
cholloro <aryabhaisare@gmail.com>
2021-07-15 18:06:36 +0530

    somehow

--- Day9 - apply call bind/third ques/script.js
+++ Day9 - apply call bind/third ques/script.js
@@ -1,13 +1,21 @@
+const Available = 36;
+const Selected = 0;
+
 function dumb(id)
 {
     var background = document.getElementById(id).style.backgroundColor;
-
     if ( background == "brown" )
     {
-        document.getElementById(id).style.background = "red"
+        document.getElementById(id).style.background = "red";
+        Available--;
+        selected++;
+      
     }
-    else
+    else if( background == "red" )
     {
         document.getElementById(id).style.background = "brown"
+        Available++;
+        selected--;
     }
 }
+ 

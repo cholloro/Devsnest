90ae07738c1c24d48fbc2b72961d6da590270931
cholloro <aryabhaisare@gmail.com>
2021-07-16 23:04:22 +0530

    idk what

--- Day9 - apply call bind/third ques/script.js
+++ Day9 - apply call bind/third ques/script.js
@@ -1,21 +1,24 @@
-const Available = 36;
-const Selected = 0;
+var Available = 36;
+var Selected = 0;
 
 function dumb(id)
 {
     var background = document.getElementById(id).style.backgroundColor;
     if ( background == "brown" )
     {
-        document.getElementById(id).style.background = "red";
-        Available--;
-        selected++;
-      
+        document.getElementById(id).style.backgroundColor = "red";
+        Available = Available-1;
+        Selected = Selected + 1;
+        document.getElementById("select").textContent=`${Selected}`
+        document.getElementById("avail").textContent=`${Available}`
     }
-    else if( background == "red" )
+    else
     {
-        document.getElementById(id).style.background = "brown"
-        Available++;
-        selected--;
+        document.getElementById(id).style.backgroundColor = "brown"
+        Available = Available + 1;
+        Selected = Selected - 1;
+        document.getElementById("select").textContent=`${Selected}`
+        document.getElementById("avail").textContent=`${Available}`
     }
 }
  
